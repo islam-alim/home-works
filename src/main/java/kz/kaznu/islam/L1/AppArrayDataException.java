75e0e2be99c8d89b24ef5e6ab27c7d2ed10bc5cf
islam-alim <islamalimzhan0@gmail.com>
2025-03-10 14:26:21 +0500

    homework done

---
+++ src/main/java/kz/kaznu/islam/L1/AppArrayDataException.java
@@ -0,0 +1,7 @@
+package kz.kaznu.islam.L1;
+
+public class AppArrayDataException extends Exception {
+    public AppArrayDataException(String message) {
+        super(message);
+    }
+}
--- src/main/java/kz/kaznu/islam/L1/MainApp.java
+++ src/main/java/kz/kaznu/islam/L1/MainApp.java
@@ -1,4 +1,44 @@
 package kz.kaznu.islam.L1;
 
+
+import java.sql.Array;
+
 public class MainApp {
+   static int total = 0;
+
+    public static void main(String[] args) {
+        String[][] array = {
+                {"1", "error", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        try {
+            sumArray(array);
+            System.out.println("Sum of the elements " + total);
+        }  catch (AppArraySizeException e) {
+            System.out.println("Size of this array is more than 4x4");
+        } catch (AppArrayDataException e) {
+            System.out.println(e.getMessage());
+
+        }
+    }
+
+    public static void sumArray(String[][] arr) throws AppArraySizeException, AppArrayDataException {
+        if (arr.length > 4) throw new AppArraySizeException();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    int a = Integer.parseInt(arr[i][j]);
+                    total += a;
+                } catch (NumberFormatException e) {
+                    throw new AppArrayDataException("Некорректные данные в ячейке (" + i + ", " + j + "): " + arr[i][j]);
+                }
+
+            }
+            if (arr[i].length > 4) throw new AppArraySizeException();
+        }
+    }
+
 }
